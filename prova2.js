529b8510306c4ea5054847e69191cb29146556dc
CarlosChaves2023 <chchaves2016@hotmail.com>
2024-03-14 09:04:43 -0300

    Inclusao do nome do usuario e conversao cm para m

--- prova2.js
+++ prova2.js
@@ -1,28 +1,41 @@
-var weight = prompt(`Digite seu peso em kg:`)
-var height = prompt(`Digite sua altura:`)
-var imc = weight / (height ** 2)
+var nome = prompt(`Digite seu nome:`)
+var heightCentimetros = parseFloat(prompt(`Digite sua altura em centímetros:`))
+var heightMetros = heightCentimetros / 100
+var weight = parseFloat(prompt(`Digite seu peso em kg:`))
+var imc = weight / (heightMetros * heightMetros)
+
+console.log(`Nome:`, nome)
+
 if (imc < 16){
     console.log(`IMC: ${imc.toFixed(2)} - Baixo peso muito grave.`)
-    alert(`IMC: ${imc.toFixed(2)} - Baixo peso muito grave.`)
+    alert(`Nome: ${nome}
+    IMC: ${imc.toFixed(2)} - Baixo peso muito grave.`)
 } else if (imc < 16.99){
     console.log(`IMC: ${imc.toFixed(2)} - Baixo peso grave`)
-    alert(`IMC: ${imc.toFixed(2)} - Baixo peso grave.`)
+    alert(`Nome: ${nome}
+    IMC: ${imc.toFixed(2)} - Baixo peso grave.`)
 } else if (imc < 18.49){
     console.log(`IMC: ${imc.toFixed(2)} - Baixo peso`)
-    alert(`IMC: ${imc.toFixed(2)} - Baixo peso.`)
+    alert(`Nome: ${nome}
+    IMC: ${imc.toFixed(2)} - Baixo peso.`)
 } else if (imc < 24.99){
     console.log(`IMC: ${imc.toFixed(2)} - Peso normal`)
-    alert(`IMC: ${imc.toFixed(2)} - Peso normal.`)
+    alert(`Nome: ${nome}
+    IMC: ${imc.toFixed(2)} - Peso normal.`)
 } else if (imc < 29.99){
     console.log(`IMC: ${imc.toFixed(2)} - Sobrepeso`)
-    alert(`IMC: ${imc.toFixed(2)} - Sobrepeso.`)
+    alert(`Nome: ${nome}
+    IMC: ${imc.toFixed(2)} - Sobrepeso.`)
 } else if (imc < 34.99){
     console.log(`IMC: ${imc.toFixed(2)} - Obesidade grau I`)
-    alert(`IMC: ${imc.toFixed(2)} - Obesidade grau I.`)
+    alert(`Nome: ${nome}
+    IMC: ${imc.toFixed(2)} - Obesidade grau I.`)
 } else if (imc < 39.99){
     console.log(`IMC: ${imc.toFixed(2)} - Obesidade grau II`)
-    alert(`IMC: ${imc.toFixed(2)} - Obesidade grau II.`)
+    alert(`Nome: ${nome}
+    IMC: ${imc.toFixed(2)} - Obesidade grau II.`)
 } else {
     console.log(`IMC: ${imc.toFixed(2)} - Obesidade grau III`)
-    alert(`IMC: ${imc.toFixed(2)} - Obesidade grau 3.`)
+    alert(`Nome: ${nome}
+    IMC: ${imc.toFixed(2)} - Obesidade grau III.`)
 }
